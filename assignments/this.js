93e1b385711ce35789e336a6d6116ffd8e39a55c
CJ Lucido <c.jose.lucido@gmail.com>
2019-09-18 11:18:18 -0700

    gave an overarching definition of 'this' in my words and one for window binding

--- assignments/this.js
+++ assignments/this.js
@@ -1,7 +1,13 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
+
+    For all forms of this I would say it is similar to the English use of it in comparison to 'that', that is 'this' points to an object that is NEAR us! If we were surrounded by only space then the concept of space would be the only object near us. In code it is like this but refers only to objects' attribute of containing things, so it points to the object nearest as a container. 
+
+    In technical terms, 'this' points to the parent scope or newly created parent scope of a container object, OR it will point to the object that you are calling or applying. 
+
+* 1. Window binding is the principle that if a declared function or function expression was defined/assigned within the global scope and 'this was used in their definitions, then 'this' refers to the window (the actual JS language) or in node it points to the global object.
+
 * 2. 
 * 3. 
 * 4. 
