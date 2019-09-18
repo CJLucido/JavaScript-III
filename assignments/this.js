1e3e84e0c2957b3422529ac12189281cc2879d3f
CJ Lucido <c.jose.lucido@gmail.com>
2019-09-18 12:50:59 -0700

    added code as an example of implicit binding, new binding, and explicit binding

--- assignments/this.js
+++ assignments/this.js
@@ -8,7 +8,7 @@
 
 * 1. Window binding is the principle that if a declared function or function expression was defined/assigned within the global scope and 'this was used in their definitions, then 'this' refers to the window (the actual JS language) or in node it points to the global object.
 
-* 2. Implicit Binding is the principle that if this is used in an object or objects method, than it points to that object itself. So 'this' will reference a property/key value pair within the object.
+* 2. Implicit Binding is the principle that if this is used in an objects method, than it points to that object itself. So 'this' will reference a property/key value pair within the object.
 
 * 3. New binding is the principle that when a constructor function is made using 'this' on the source objects key value pairs, then when a new object is created using that constructor function, 'this' will refer to that new object and not the source object or the constructor function.
 
@@ -19,6 +19,8 @@
 
 // Principle 1
 
+//code example for Window Binding
+
 function atGlbl(){
     console.log(this.world)
 };
@@ -27,16 +29,55 @@ const world = "won't be this";
 
 atGlbl(); //browser seems to be defaulting "use strict" because undefined is coming back instead of window
 
-// code example for Window Binding
-
 // Principle 2
 
 // code example for Implicit Binding
 
+let principal2 = {
+    name: "new object",
+    location: "place",
+    activity: function(){
+        console.log(`${this.name} at ${this.location}`)
+    },
+};
+
+principal2.activity();
+
 // Principle 3
 
-// code example for New Binding
+//code example for New Binding
+
+function Principal3(props){
+    this.name = props.name;
+    this.location = props.location;
+    this.year = props.year;
+    this.explain = function(){
+        return `${this.name} was bound to her husband ${this.location} in ${this.year}`
+    }
+};
+
+const newlyWed = new Principal3(
+            {
+            name:"Mary Lee Bound",
+            location:"three steps down",
+            year:1999
+        }
+    );
+
+console.log(newlyWed.explain());
 
 // Principle 4
 
 // code example for Explicit Binding
+
+
+
+const oldlyWed = new Principal3(
+    {
+    name:"Marie Turner Roun",
+    location:"back up town",
+    year: 1867
+}
+);
+
+console.log(principal2.activity.call(oldlyWed));
